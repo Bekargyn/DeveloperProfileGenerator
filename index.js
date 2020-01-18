37914619c1c505b6307180227c5511e1764f6b8f
Bek Abdrakhmanov <Bekargyn@gmail.com>
2020-01-17 23:32:36 -0600

    added html file reader

--- index.js
+++ index.js
@@ -7,16 +7,14 @@ function makePDF(data) {
   let pdf = require("html-pdf");
   data.style = fs.readFileSync("style.css");
   let html = fs.readFileSync("index.html");
-
-  pdf
-    .create(
-      html.toString().replace(/\${([^}]*)}/g, (r, k) => data[k]),
-      options
-    )
-    .toFile("./businesscard.pdf", function(err, res) {
-      if (err) return console.log(err);
-      console.log(res); // { filename: '/app/businesscard.pdf' }
-    });
+  html = html.toString().replace(/\${([^}]*)}/g, (r, k) => data[k]);
+  pdf.create(html, options).toFile("./businesscard.pdf", function(err, res) {
+    if (err) return console.log(err);
+    console.log(res); // { filename: '/app/businesscard.pdf' }
+  });
+  fs.writeFile("profile.html", html, function(err) {
+    //console.log(err);
+  });
 }
 
 function promptUser() {
@@ -36,32 +34,37 @@ function promptUser() {
     ])
 
     .then(function(data) {
+      data.color == "white"
+        ? (data.textcolor = "black")
+        : (data.textcolor = "white");
+
       // Make a request for a user with a given ID
-      axios
-        .get(`https://api.github.com/users/${data.username}`)
-        // .get(`https://api.github.com/users/${data.username}/starred`)
-        .then(function(response) {
-          data.name = response.data.name;
-          data.public_repos = response.data.public_repos;
-          data.followers = response.data.followers;
-          data.following = response.data.following;
-          data.avatar_url = response.data.avatar_url;
-          data.location = response.data.location;
-          makePDF(data);
-        });
+      const request1 = axios.get(
+        `https://api.github.com/users/${data.username}`
+      );
+      const request2 = axios.get(
+        `https://api.github.com/users/${data.username}/starred`
+      );
+      // .get(`https://api.github.com/users/${data.username}/starred`)
+
+      axios.all([request1, request2]).then(function(response) {
+        data.name = response[0].data.name;
+        data.public_repos = response[0].data.public_repos;
+        data.bio = response[0].data.bio
+          ? response[0].data.bio
+          : "I don't have bios";
+        data.followers = response[0].data.followers;
+        data.following = response[0].data.following;
+        data.avatar_url = response[0].data.avatar_url;
+        data.location = response[0].data.location
+          ? response[0].data.location
+          : "N/A";
+        data.starred = response[1].data.length;
+        makePDF(data);
+      });
     });
 }
 
 promptUser();
 
 // need to use 'Promise'?
-
-// ###############
-function init() {
-  const fileHTML = generateHTML();
-  console.log(fileHTML);
-  fs.writeFile("profile.html", fileHTML, function(err) {
-    console.log(err);
-  });
-}
-// init();
